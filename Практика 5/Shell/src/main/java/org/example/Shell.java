24497d2141b0fbb3af7a0ea9242366a03af9c53d
Kentolf <m.morgun.dima2007@yandex.ru>
2025-03-25 10:09:47 +0300

    Добавил рефлексию

--- Практика 5/Shell/src/main/java/org/example/Shell.java
+++ Практика 5/Shell/src/main/java/org/example/Shell.java
@@ -1,36 +1,34 @@
 package org.example;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class Shell {
+    private final Map<String, Command> commands;
+
+    public Shell() {
+        commands = SearchClasses.loadCommands("org.example");
+    }
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("> ");
-            String input = scanner.nextLine().trim();
+            String input = scanner.nextLine().trim().toLowerCase(); // читаем и преобразуем команду
 
-            switch (input) {
-                case "date":
-                    new DateCommand().execute();
-                    break;
-                case "time":
-                    new TimeCommand().execute();
-                    break;
-                case "pwd":
-                    new PwdCommand().execute();
-                    break;
-                case "exit":
-                    new ExitCommand().execute();
-                    return;
-                case "help":
-                    new HelpCommand().execute();
-                    break;
-                default:
-                    System.out.println("Invalid syntax!");
-                    break;
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            Command command = commands.get(input);
+            if (command != null) {
+                command.execute();
+            } else {
+                System.out.println("Invalid command!");
             }
         }
+
+        System.out.println("The program has completed its work");
     }
 
     public static void main(String[] args) {
