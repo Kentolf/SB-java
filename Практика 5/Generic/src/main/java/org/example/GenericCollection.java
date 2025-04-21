caa56b8a1664f9a4052483e6ad7ffd9fdd02756a
Kentolf <m.morgun.dima2007@yandex.ru>
2025-04-22 01:16:24 +0300

    Убрал array collection и реализовал свой вариант

--- Практика 5/Generic/src/main/java/org/example/GenericCollection.java
+++ Практика 5/Generic/src/main/java/org/example/GenericCollection.java
@@ -1,41 +1,67 @@
 package org.example;
 
-import java.util.ArrayList;
-
 public class GenericCollection<T> {
-    private ArrayList<T> collection;
+
+    private Object[] elements;
+    private int size;
 
     public GenericCollection() {
-        this.collection = new ArrayList<>();
+        
+        elements = new Object[10];
+        size = 0;
     }
 
     public void add(T element) {
-        collection.add(element);
+
+        capacity();
+        elements[size++] = element;
     }
 
     public void remove(int index) {
-        if (index < 0 || index >= collection.size()) {
+
+        if (index < 0 || index >= size) {
             System.err.println("Invalid index: " + index);
             return;
         }
-        collection.remove(index);
+
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+
+        elements[--size] = null;
     }
 
     public T get(int index) {
-        if (index < 0 || index >= collection.size()) {
+
+        if (index < 0 || index >= size) {
             System.err.println("Invalid index: " + index);
             return null;
         }
-        return collection.get(index);
+
+        return (T) elements[index];
     }
 
     public int size() {
-        return collection.size();
+        return size;
     }
 
     public void printAll() {
-        for (T element : collection) {
-            System.out.println(element);
+
+        for (int i = 0; i < size; i++) {
+            System.out.println(elements[i]);
+        }
+    }
+
+    private void capacity() {
+
+        if (size == elements.length) {
+            Object[] newArray = new Object[elements.length + 1];
+
+            for (int i = 0; i < elements.length; i++) {
+                newArray[i] = elements[i];
+            }
+
+            elements = newArray;
         }
     }
 }
