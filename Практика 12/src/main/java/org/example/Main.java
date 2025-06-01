3b5840cc2e8ef21dfaa69a7300f4d62c3d024c2f
Kentolf <m.morgun.dima2007@yandex.ru>
2025-06-02 01:59:55 +0300

    Исправил код

--- Практика 12/src/main/java/org/example/Main.java
+++ Практика 12/src/main/java/org/example/Main.java
@@ -2,95 +2,181 @@ package org.example;
 
 import java.io.*;
 import java.util.regex.*;
+import java.nio.file.Files;
 
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length < 2 || args.length > 3) {
-            System.err.println("Usage: java Analyzer [-i] <pattern> <file_or_directory>");
+        PatternSearcher searcher = new PatternSearcher();
+        searcher.run(args);
+    }
+}
+
+class PatternSearcher {
+
+    private boolean ignoreCase;
+    private String patternStr;
+    private String pathStr;
+    private Pattern pattern;
+
+    public void run(String[] args) {
+
+        if (!parseArgs(args)) {
             return;
         }
 
-        boolean ignoreCase = false;  // с учетом / без учета регистра (по умолчанию без)
-        String patternStr;  // сам паттерн (пока только как строка)
-        String pathStr;  // путь (тоже пока что строка)
+        File path = new File(pathStr);
+        if (!isPath(path)) {
+            return;
+        }
 
-        if (args.length == 2) {  // нет -i, значит регистр учитывается
+        if (!Pattern()) {
+            return;
+        }
+        processPath(path);
+    }
+
+    private boolean parseArgs(String[] args) {
+
+        if (args.length < 2 || args.length > 3) {
+            System.out.println("Usage: java Analyzer [-i] <pattern> <file_or_directory>");
+            return false;
+        }
+
+        if (args.length == 2) {
             patternStr = args[0];
             pathStr = args[1];
+            ignoreCase = false;
         }
         else {
-            if (!args[0].equals("-i")) {
-                System.err.println("Unknown option: " + args[0]);
-                return;
+            if (args[0].equals("-i")) {
+                ignoreCase = true;
+                patternStr = args[1];
+                pathStr = args[2];
             }
-            ignoreCase = true;
-            patternStr = args[1];
-            pathStr = args[2];
+            else {
+                System.out.println("Unknown option: " + args[0]);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isPath(File path) {
+
+        if (!path.exists()) {
+            System.out.println("Такого пути не существует");
+            return false;
         }
 
-        File path = new File(pathStr);  // получаем путь из строки
-        if (!path.exists()) {
-            System.err.println("Error: path does not exist.");
+        if (!path.isFile() && !path.isDirectory()) {
+            System.out.println("Такого файла/директории не существует");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean Pattern() {
+
+        try {
+            if (ignoreCase) {
+                pattern = Pattern.compile(patternStr, Pattern.UNICODE_CASE);
+            }
+            else {
+                pattern = Pattern.compile(patternStr);
+            }
+
+            return true;
+        }
+        catch (PatternSyntaxException e) {
+            System.out.println("Неправильный паттерн");
+            return false;
+        }
+    }
+
+    private void processPath(File path) {
+
+        if (path.isFile()) {
+            searchInFile(path, false);
+        }
+
+        else if (path.isDirectory()) {
+            processDirectory(path);
+        }
+    }
+
+    private void processDirectory(File directory) {
+
+        File[] files = directory.listFiles();
+
+        if (files == null) {
+            System.out.println("Директория пуста");
             return;
         }
 
-        if (path.isFile()) {  // если путь - это файл
-            if (ignoreCase) { // если игнорируем регистр, переходим к созданию паттерна без регистра
-                processFileIgnoreCase(path, patternStr, false); // не показываем название файла, если он один
-            } else {  // не игнорируем
-                processFileBaseCase(path, patternStr, false);
+        for (File file : files) {
+            if (file.isFile()) {
+                searchInFile(file, true);
             }
-        } else if (path.isDirectory()) { // то же самое, но для файлов из директории
-            File[] files = path.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    if (file.isFile()) {
-                        if (ignoreCase) {
-                            processFileIgnoreCase(file, patternStr, true);
-                        } else {
-                            processFileBaseCase(file, patternStr, true);
-                        }
-                    }
-                }
-            } else {
-                System.err.println("Error: Unable to list files in directory.");
-            }
-        } else {
-            System.err.println("Error: Not a file or directory.");
         }
     }
 
-    public static void processFileBaseCase(File file, String patternStr, boolean showFileName) {
-        Pattern pattern = Pattern.compile(patternStr);  // создаем паттерн учитывая регистр
-        processFileWithPattern(file, pattern, showFileName);
-    }
+    private void searchInFile(File file, boolean showFileName) {
 
-    public static void processFileIgnoreCase(File file, String patternStr, boolean showFileName) {
-        Pattern pattern = Pattern.compile(patternStr, Pattern.CASE_INSENSITIVE);  // создаем паттерн не учитывая регистр
-        processFileWithPattern(file, pattern, showFileName);
-    }
+        if (isBinary(file)) {
+            return;
+        }
 
-    private static void processFileWithPattern(File file, Pattern pattern, boolean showFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
+
             int lineNumber = 1;
-            while ((line = reader.readLine()) != null) {  // обрабатываем файл в цикле
+
+            while ((line = reader.readLine()) != null) {
                 Matcher matcher = pattern.matcher(line);
-                if (matcher.find()) {  // находим совпадение с паттерном - выводим строку и файл
-                    String prefix;
-                    if (showFileName) {  // если работаем с несколькими файлами, выводим название текущего
-                        prefix = file.getName() + ":";
-                    }
-                    else {
-                        prefix = "";  // если с одним - не выводим
-                    }
-                    System.out.println(prefix + lineNumber + ": " + line);
+
+                if (matcher.find()) {
+                    Match(file, showFileName, lineNumber, line);
                 }
                 lineNumber++;
             }
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + file.getName());
+        }
+        catch (IOException e) {
+            System.out.println("Ошибка с чтением файла: " + file.getName());
+        }
+    }
+
+    private void Match(File file, boolean showFileName, int lineNumber, String line) {
+
+        String prefix;
+
+        if (showFileName) {
+            prefix = file.getName() + ":";
+        }
+        else {
+            prefix = "";
+        }
+        System.out.println(prefix + lineNumber + ": " + line);
+    }
+
+    private boolean isBinary(File file) {
+
+        try (InputStream in = Files.newInputStream(file.toPath())) {
+
+            byte[] buffer = new byte[512];
+            int bytesRead = in.read(buffer);
+
+            for (int i = 0; i < bytesRead; i++) {
+                if (buffer[i] == 0) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        catch (IOException e) {
+            System.out.println("Ошибка с чтением файла: " + file.getName());
+            return true;
         }
     }
 }
